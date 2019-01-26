2ecef54ce179e472d85ac5e6dff368c21e235cb8
Lukasz Galek <lukasz.galek@o2.pl>
2019-01-26 13:38:16 +0100

    next commit

--- src/main/java/SDA/App.java
+++ src/main/java/SDA/App.java
@@ -1,5 +1,8 @@
 package SDA;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -11,14 +14,38 @@ public class App
       User user1 = new User("lukas","galek","mail","jakies haslo");
       User user2 = new User("krzysiek","zdzisiek","mail2","jakies cos");
 //        User user1 = new User();
-        System.out.println(user1.getImie());
+       // System.out.println(user1.getImie());
 //       user1.setImie("lukas");
-        System.out.println("cos \n");
+        //System.out.println("cos \n");
+
+        List<String> users = new ArrayList<>();
+        users.add("jan");
+        users.add("krzysiek");
+        System.out.println(users);
+        for (String user:users){
+            System.out.println(user + "\n");}
+
 
         UserService service = new UserService();
 
-        service.addUser(user1);
-        service.addUser(user2);
-        service.printAll();
-    }
-}
+//        service.addUser(user1);
+//        service.addUser(user2);
+//        service.printAll();
+
+        List<Integer> liczby = new ArrayList<>();
+        liczby.add(2);
+        liczby.add(3);
+        liczby.add(1);
+        for (Integer liczba:liczby){
+            System.out.println(liczba);
+        }
+
+        List<User> user = new ArrayList<>();
+        user.add(user1);
+        user.add(user2);
+
+        for (User dane:user){
+            System.out.println(dane);}
+
+    }}
+
